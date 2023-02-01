a380e2442f72f7e731a98dbd7212110842e6b7af
pranavsinghrio <pranavsinghruhela@gmail.com>
2023-02-01 20:33:05 +0530

    message

---
+++ src/main/java/com/example/BookMyShowProject/Repositories/ShowRepository.java
@@ -0,0 +1,8 @@
+package com.example.BookMyShowProject.Repositories;
+
+
+import com.example.BookMyShowProject.Models.Show;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShowRepository extends JpaRepository<Show,Integer> {
+}
---
+++ src/main/java/com/example/BookMyShowProject/Repositories/ShowSeatsRepository.java
@@ -0,0 +1,8 @@
+
+package com.example.BookMyShowProject.Repositories;
+
+import com.example.BookMyShowProject.Models.ShowSeats;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShowSeatsRepository extends JpaRepository<ShowSeats,Integer> {
+}
